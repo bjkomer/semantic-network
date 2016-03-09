65566633e504598dd75930704bb28c4864c870c9
Brent Komer <brent.komer@gmail.com>
2016-03-09 11:36:18 -0500

    fixing write to be read

--- view_cifar_h5.py
+++ view_cifar_h5.py
@@ -8,7 +8,11 @@ import time
 fnametrain = 'data/cifar_100_caffe_hdf5/train.h5'
 fnametest = 'data/cifar_100_caffe_hdf5/test.h5'
 
-ftrain = h5py.File(fnametrain, 'w')
+ftrain = h5py.File(fnametest, 'r')
+#ftrain = h5py.File(fnametest, 'r', compression='gzip')
+print(dir(ftrain))
+print(ftrain.keys())
+print(ftrain.values())
 print(ftrain['data'])
 print(ftrain['label_coarse'])
 print(ftrain['label_fine'])
