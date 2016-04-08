ce25b2e3cd6f4235b8671783accc0b9a1720b440
Brent Komer <brent.komer@gmail.com>
2016-04-08 15:12:29 -0400

    flexible method of training w2v with different dimensions

--- train_word2vec.py
+++ train_word2vec.py
@@ -1,12 +1,18 @@
 import word2vec
+import sys
+
+if len(sys.argv) == 2:
+    dim = int(sys.argv[1])
+else:
+    dim = 100
 
 word2vec.word2phrase('/home/bjkomer/word2vec/text8',
                      '/home/bjkomer/semantic-network/data/text8-phrases', verbose=True)
 
 word2vec.word2vec('/home/bjkomer/semantic-network/data/text8-phrases',
-                  '/home/bjkomer/semantic-network/data/text8.bin', size=100,
+                  '/home/bjkomer/semantic-network/data/text8-%s.bin'%dim, size=dim,
                   verbose=True)
 
 word2vec.word2clusters('/home/bjkomer/word2vec/text8',
-                       '/home/bjkomer/semantic-network/data/text8-clusters.txt', 100,
+                       '/home/bjkomer/semantic-network/data/text8-%s-clusters.txt'%dim, dim,
                        verbose=True)
