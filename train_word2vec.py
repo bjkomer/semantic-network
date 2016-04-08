c2a4a638c26aebc5e8d4eb41fad2b2ea715e035c
Brent Komer <brent.komer@gmail.com>
2016-04-08 15:17:34 -0400

    script works across computers

--- train_word2vec.py
+++ train_word2vec.py
@@ -1,18 +1,25 @@
 import word2vec
 import sys
+import getpass
+
+user = getpass.getuser()
+if user == 'ctnuser':
+    root = '/home/ctnuser/bjkomer/'
+elif user == 'bjkomer':
+    root = '/home/bjkomer/'
 
 if len(sys.argv) == 2:
     dim = int(sys.argv[1])
 else:
     dim = 100
 
-word2vec.word2phrase('/home/bjkomer/word2vec/text8',
-                     '/home/bjkomer/semantic-network/data/text8-phrases', verbose=True)
+word2vec.word2phrase(root + 'word2vec/text8',
+                     root + 'semantic-network/data/text8-phrases', verbose=True)
 
-word2vec.word2vec('/home/bjkomer/semantic-network/data/text8-phrases',
-                  '/home/bjkomer/semantic-network/data/text8-%s.bin'%dim, size=dim,
+word2vec.word2vec(root + 'semantic-network/data/text8-phrases',
+                  root + 'semantic-network/data/text8-%s.bin'%dim, size=dim,
                   verbose=True)
 
-word2vec.word2clusters('/home/bjkomer/word2vec/text8',
-                       '/home/bjkomer/semantic-network/data/text8-%s-clusters.txt'%dim, dim,
+word2vec.word2clusters(root + 'word2vec/text8',
+                       root + 'semantic-network/data/text8-%s-clusters.txt'%dim, dim,
                        verbose=True)
